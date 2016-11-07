29755e347473821ae63065dd3b97829541c543df
NIRANJAN KUMAR <justniranjan1994@gmail.com>
2016-11-08 01:02:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,6 +122,10 @@ app.get('/articles/:articleName',function(req,res){
 
 });
 
+app.get('/blog/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'blog', 'index.html'));
+});
+
 app.get('/blog/css/bootstrap.min.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'blog/css', 'bootstrap.min.css'));
 });
