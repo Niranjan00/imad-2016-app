b45fbdf6463c22a39c95ce4e3708a58e82d2575e
NIRANJAN KUMAR <justniranjan1994@gmail.com>
2016-11-11 00:54:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user : 'niranjan00',
@@ -16,6 +17,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 
 
@@ -72,6 +74,24 @@ app.get('/hash/:input',function(req,res){
     var hashString = hash(req.params.input, 'this-is-a-random-stirng');
     res.send(hashString);
 })
+
+
+app.post('/create-user', function (req, res) {
+// username, password
+// {"username": "niranjan", "password": "password"}
+// JSON
+var username = req.body.username;
+var password = req.body.password;
+var salt = crypto.randomBytes(128).toString('hex');
+var dbString = hash(password, salt);
+pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+if (err) {
+res.status(500).send(err.toString());
+} else {
+res.send('User successfully created: ' + username);
+}
+});
+});
 
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
