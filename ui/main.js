2dea147fa3f96fdabdc2c83b4ce48e427f416b18
NIRANJAN KUMAR <justniranjan1994@gmail.com>
2016-11-25 00:56:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+
 function loadArticles () {
         // Check if the user is already logged in
     var request = new XMLHttpRequest();
@@ -5,15 +6,19 @@ function loadArticles () {
         if (request.readyState === XMLHttpRequest.DONE) {
             var articles = document.getElementById('articles');
             if (request.status === 200) {
-                var content = '<ul>';
                 var articleData = JSON.parse(this.responseText);
+                var content = '';
                 for (var i=0; i< articleData.length; i++) {
-                    content += `<li>
-                    <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
-                    (${articleData[i].date.split('T')[0]})</li>`;
+                     content += `
+                                <a href="/${articleData[i].title}">
+                                <h2 class="post-title">
+                                     ${articleData[i].heading}
+                                </h2>
+                                
+                                </a>
+                    <p class="post-meta">Posted by <a href="/about.html">${articleData[i].author}</a> on (${articleData[i].date.split('T')[0]})</p>`;
                 }
-                content += "</ul>"
-                articles.innerHTML = content;
+               articles.innerHTML = content;
             } else {
                 articles.innerHTML('Oops! Could not load all articles!')
             }
@@ -23,6 +28,5 @@ function loadArticles () {
     request.open('GET', '/get-articles', true);
     request.send(null);
 }
-
 // Now this is something that we could have directly done on the server-side using templating too!
 loadArticles();
