444eba34879cf9114793032f1f451fe00de3bfd2
NIRANJAN KUMAR <justniranjan1994@gmail.com>
2016-11-25 01:48:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,13 +38,13 @@ function loadCategory() {
                 var content = '<ul>';
                 var categoryData = JSON.parse(this.responseText);
                 for (var i=0; i< categoryData.length; i++) {
-                     content += `<li>
-                                <a href="">
-                                    <h4>${categoryData[i].name}</h4>
-                                </a></li>`;
+                    //  content += `<li>
+                    //             <a href="">
+                    //                 <h4>${categoryData[i].name}</h4>
+                    //             </a></li>`;
                 }
                content += "</ul>"
-               categories.innerHTML = content;
+               categories.innerHTML = Hello;
             } else {
                 categories.innerHTML('Oops! Could not load all category!')
             }
