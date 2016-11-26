b1a2993286bd61f8396fef4d3c96bb2b923c8c55
NIRANJAN KUMAR <justniranjan1994@gmail.com>
2016-11-26 23:38:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get('/check-login', function (req, res) {
 
 app.get('/logout', function (req, res) {
    delete req.session.auth;
-   res.status(200).send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
+   res.status(200).sendFile(path.join(__dirname, 'ui', 'blog.html'));
 });
 
 var pool = new Pool(config);
