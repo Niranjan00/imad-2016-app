8a715c99e2f20f9885943dcd3761f13531ff26a3
NIRANJAN KUMAR <justniranjan1994@gmail.com>
2016-11-29 23:43:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,18 @@
 
 function loadLoginForm () {
-         var loginHtml = ` <h3>Login/Register</h3>
-        <div class="row control-group">
-            <div class="form-group col-xs-12 floating-label-form-group controls">
-              <label>Username</label>
-                <input type="text" class="form-control" id="username" placeholder="Enter Your Username" required>
-            </div>
-        </div>
-        <div class="row control-group">
-            <div class="form-group col-xs-12 floating-label-form-group controls">
-              <label>Password</label>
-                <input type="password" class="form-control" id="password" placeholder="Password" required>
-            </div>
-        </div>
-        <br/>
-        <input type="submit" class="btn btn-default" id="login_btn" value="Login" />
-        <input type="submit" class="btn btn-default" id="register_btn" value="Register" />
-        `;
-             
+        var loginHtml = `
+                <h3>Login/Reister</h3>
+                
+                   <div class="form-group">
+                        <input type="text" class="form-control" id="username" placeholder="User Name" required>
+                   </div>
+                   <div class="form-group">
+                       <input type="password" class="form-control" id="password" placeholder="Password" required>
+                   </div>
+                     <button type="submit" class="btn btn-info" id="login_btn">Login</button>
+                     <button type="submit" class="btn btn-info" id="register_btn">Register</button>
+               
+        `;    
     document.getElementById('login_area').innerHTML = loginHtml;
     
     // Submit username/password to login
